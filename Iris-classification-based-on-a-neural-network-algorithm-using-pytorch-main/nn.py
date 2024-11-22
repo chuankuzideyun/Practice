01a1b38f0349ef0a8534d9ba22211a7dcda3e247
chuankuzideyun <yn88071@163.com>
2024-11-22 09:47:43 +0100

    Finished

--- Iris-classification-based-on-a-neural-network-algorithm-using-pytorch-main/nn.py
+++ Iris-classification-based-on-a-neural-network-algorithm-using-pytorch-main/nn.py
@@ -4,15 +4,15 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm #显示进度条
 
 import torch
-import orch.nn as nn
+import torch.nn as nn
 import torch.optim as optim
 
 from data_loader import iris_dataload
 
 #初始化神经网络模型
-class NN(nn.Modle):
-  def _init_(self,in_dim, hidden_dim1,hidden_dim2, out_dim): #神经网络三个层级
-    super()._init_()
+class NN(nn.Module):
+  def __init__(self,in_dim, hidden_dim1,hidden_dim2, out_dim): #神经网络三个层级
+    super().__init__()
     self.layer1 = nn.Linear(in_dim, hidden_dim1)
     self.layer2 = nn.Linear(hidden_dim1, hidden_dim2)
     self.layer3 = nn.Linear(hidden_dim2, out_dim)
@@ -36,7 +36,7 @@ train_dataset, val_dataset, test_dataset = torch.utils.data.random_split(custom_
 train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True) #每次抽取16个数据，下次抽取之前把所有数据打散重排
 val_loader = DataLoader(val_dataset, batch_size=1, shuffle=False)
 test_loader = DataLoader(test_dataset, batch_size=16, shuffle=True)
-print("训练集大小", len(train_loader)*16, "验证集大小", len(train_loader)*16, "测试集大小", len(train_loader)*16)
+print("训练集大小", len(train_loader)*16, "验证集大小", len(train_loader), "测试集大小", len(train_loader))
 
 #定义推理函数，计算并返回准确率
 def infer(model, dataset, device):
@@ -51,10 +51,10 @@ def infer(model, dataset, device):
       acc_num += torch.eq(predict_y, label.to(device)).sum().item()
     acc = acc_num/len(dataset) #正确数量acc/数据总量
     return acc
-  
+
 def main(lr=0.005, epochs=20):
   model = NN(4,12,6,3).to(device) #中间是超参数，可以更改
-  lose_f = nn.CrossEntropyLoss()
+  loss_f = nn.CrossEntropyLoss()
   pg = [p for p in model.parameters() if p.requires_grad] #三目运算语法
   optimizer = optim.Adam(pg, lr=lr) #优化器，对数据做更新
   #权重文件储存器
@@ -63,7 +63,7 @@ def main(lr=0.005, epochs=20):
     os.makedirs(save_path)
   
   #开始训练
-  for epoch in epochs:
+  for epoch in range(epochs):
     model.train()
     acc_num = torch.zeros(1).to(device) #维度为1
     sample_num = 0
@@ -84,7 +84,20 @@ def main(lr=0.005, epochs=20):
       loss.backward()
       optimizer.step()
 
+      #进度条
       train_acc = acc_num/sample_num
-      train_bar.desc = "train epoch[{}/{}] loss:{: .3f}".format(epoch + 1, epochs, loss)
+      train_bar.desc = "train epoch[{}/{}] loss:{:.3f}".format(epoch + 1, epochs, loss)
     val_acc = infer(model, val_loader, device)
-    print("train epoch[{}/{}] loss:{: .3f} train_acc{: .3f}".format(epoch + 1, epochs, loss, train_acc))
+    print("train epoch[{}/{}] loss:{: .3f} train_acc{:.3f} val_acc{:.3f}".format(epoch + 1, epochs, loss, train_acc, val_acc))
+    torch.save(model.state_dict(), os.path.join(save_path, "nn.pth"))
+
+    #每次数据集迭代之后，对初始化指标清零
+    train_acc = 0
+    val_acc = 0
+  print("Finished Training")
+
+  test_acc = infer(model, test_loader, device)
+  print("test_acc", test_acc)
+
+if __name__ == "__main__":
+  main()
